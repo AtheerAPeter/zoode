359028ff14065d1a34cd5e17635d5e80b709639c
Atheer Adeeb <60075895+AtheerAdeeb@users.noreply.github.com>
2020-10-04 19:19:20 +0300

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+//using live sass compiler vs code plugin
+import "../css/style.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
